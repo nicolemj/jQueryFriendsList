86001019ea9bbcc1a059c6c4f3545774ee5df08c
Nicole jeffries <nicolejeffries0118@gmail.com>
2018-01-16 18:24:42 -0500

    jQueryFriendsList

---
+++ main.js
@@ -0,0 +1,18 @@
+    $(document).ready(function() {
+
+    $("#friend-form").submit( (e)=> {
+        e.preventDefault();
+        let newFriend = $('#input').val();
+        let newElement = createFriend(newFriend);
+        $('#friend-list').append(newElement);
+        $('#input').val("");
+    })
+
+    $('#friend-list').on('click', 'li', function(e) {
+        $(this).remove();
+    })
+
+    function createFriend(name) {
+        return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+    }
+    });
